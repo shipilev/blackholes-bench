01825cac0b4d258135ea70f76cd14537142d627d
Aleksey Shipilev <shade@redhat.com>
2020-09-17 19:08:19 +0200

    Initial import

---
+++ src/main/java/org/openjdk/XplusY.java
@@ -0,0 +1,24 @@
+package org.openjdk;
+
+import org.openjdk.jmh.annotations.*;
+import org.openjdk.jmh.infra.Blackhole;
+
+import java.util.concurrent.TimeUnit;
+
+@Warmup(iterations = 5, time = 1, timeUnit = TimeUnit.SECONDS)
+@Measurement(iterations = 5, time = 1, timeUnit = TimeUnit.SECONDS)
+@Fork(1)
+@BenchmarkMode(Mode.AverageTime)
+@OutputTimeUnit(TimeUnit.NANOSECONDS)
+@State(Scope.Benchmark)
+public class XplusY {
+
+    int x, y;
+
+    @Benchmark
+    public void test(Blackhole bh) {
+        bh.consume(x + y);
+        bh.consume(x + y);
+    }
+
+}
